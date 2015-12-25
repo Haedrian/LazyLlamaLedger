527c2d85d1bae3d14e92f6433936154c17ec301a
Adrian Abela <aadrianabela@gmail.com>
2015-12-25 01:45:31 +0100

    Now opens LLL on startup

--- LazyLlamaLedger/LazyLlamaLedger/Startup.cs
+++ LazyLlamaLedger/LazyLlamaLedger/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Owin;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +27,9 @@ namespace LazyLlamaLedger
             );
 
             appBuilder.UseWebApi(config);
+
+            //Open the browser
+            System.Diagnostics.Process.Start(Directory.GetCurrentDirectory() + "/../../LazyLlamaLedger.htm");
         }
     }
 }
