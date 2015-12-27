46d851e82a5a00a5e5501262dceba8f12745cc8d
Adrian <aadrianabela@gmail.com>
2015-12-27 15:34:51 +0100

    Interface prepped for Categories

---
+++ LazyLlamaLedger/WebApp/js/categories.js
@@ -0,0 +1,8 @@
+﻿$(document).on('keypress', '[contenteditable=true]', function (e) {
+    return e.which != 13;
+});
+
+$(document).on('paste','[contenteditable=true]',function (e)
+{
+    e.preventDefault();
+})
---
+++ LazyLlamaLedger/WebApp/js/common.js
@@ -0,0 +1,21 @@
+﻿///Closes all sections
+function closeAll()
+{
+    $("#divCategories").css("display", "none");
+    $("#divLedger").css("display", "none");
+    $("li.active").removeClass("active");
+}
+
+function openLedger()
+{
+    closeAll();
+    $("#divLedger").css("display", "block");
+    $("#liLedger").addClass("active");
+}
+
+function openCats()
+{
+    closeAll();
+    $("#divCategories").css("display", "block");
+    $("#liCategories").addClass("active");
+}
