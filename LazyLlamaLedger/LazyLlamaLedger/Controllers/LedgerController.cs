f90d2cb2a08a539fc7a1723c661464cd7f7c90db
Adrian Abela <aadrianabela@gmail.com>
2015-12-29 09:50:14 +0100

    Added controller method for Income Aggregation

--- LazyLlamaLedger/LazyLlamaLedger/Controllers/LedgerController.cs
+++ LazyLlamaLedger/LazyLlamaLedger/Controllers/LedgerController.cs
@@ -35,11 +35,25 @@ namespace LazyLlamaLedger.Controllers
         /// <returns></returns>
         [HttpGet]
         [ActionName("LedgerExpAggregate")]
-        public IHttpActionResult GetAggregate(int month, int year)
+        public IHttpActionResult GetExpenseAggregate(int month, int year)
         {
             //First get all the values of that month
 
             var monthValues = DataHandling.LedgerEntries.Where(e => e.Date.Month == month && e.Date.Year == year && e.IsExpense).ToArray();
+
+            //Then group them by category and return the cat and the sum
+            var res = monthValues.GroupBy(m => m.Category).Select(cg => new { Category = DataHandling.Categories.Where(c => c.ID == cg.First().Category).Select(c => c.Name).FirstOrDefault(), Sum = cg.Sum(cg1 => cg1.Money).ToString("0.00") });
+
+            return Ok(res);
+        }
+
+        [HttpGet]
+        [ActionName("LedgerIncAggregate")]
+        public IHttpActionResult GetIncomeAggregate(int month, int year)
+        {
+            //First get all the values of that month
+
+            var monthValues = DataHandling.LedgerEntries.Where(e => e.Date.Month == month && e.Date.Year == year && !e.IsExpense).ToArray();
 
             //Then group them by category and return the cat and the sum
             var res = monthValues.GroupBy(m => m.Category).Select(cg => new { Category = DataHandling.Categories.Where(c => c.ID == cg.First().Category).Select(c => c.Name).FirstOrDefault(), Sum = cg.Sum(cg1 => cg1.Money).ToString("0.00") });
