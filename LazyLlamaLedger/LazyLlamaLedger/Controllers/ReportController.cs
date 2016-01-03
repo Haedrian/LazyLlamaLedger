46bc55e98f67fd4d0c974ba8f584f729f8ba2c23
Adrian Abela <aadrianabela@gmail.com>
2016-01-03 22:49:12 +0100

    Tiny bit of scaffolding around report table

---
+++ LazyLlamaLedger/LazyLlamaLedger/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace LazyLlamaLedger.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class ReportController
+        : ApiController
+    {
+
+        public IHttpActionResult GetTable(DateTime startDate,DateTime endDate)
+        {
+            return Ok(); //TODO
+        }
+
+    }
+}
---
+++ LazyLlamaLedger/LazyLlamaLedger/Models/ReportTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyLlamaLedger.Models
+{
+    public class ReportTable
+    {
+        public List<ReportRow> row;
+    }
+
+    public class ReportRow
+    {
+        public string categoryName;
+        public List<decimal> values;
+    }
+}
