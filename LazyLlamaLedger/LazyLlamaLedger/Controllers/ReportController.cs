78e65dd8fab95d9c53649a65012473e2eb45f72f
Adrian Abela <aadrianabela@gmail.com>
2016-01-05 22:41:20 +0100

    Added Income Report.

--- LazyLlamaLedger/LazyLlamaLedger/Controllers/ReportController.cs
+++ LazyLlamaLedger/LazyLlamaLedger/Controllers/ReportController.cs
@@ -86,5 +86,76 @@ namespace LazyLlamaLedger.Controllers
             return Ok(rt); //TODO
         }
 
+        [HttpGet]
+        [ActionName("IncomeTable")]
+        public IHttpActionResult GetIncomeTable(DateTime startDate, DateTime endDate)
+        {
+            //So, first we get every category we have
+            var allCats = DataHandling.Categories;
+
+            //Create an empty report row for each
+            ReportTable rt = new ReportTable();
+            rt.Rows = new List<ReportRow>();
+            rt.Headers = new List<string>();
+
+            foreach (var cat in allCats)
+            {
+                ReportRow rr = new ReportRow();
+                rr.CategoryName = cat.Name;
+                rr.CatID = cat.ID; //for easier matching
+                rr.Values = new List<string>();
+
+                rt.Rows.Add(rr);
+            }
+
+            LedgerController lc = new LedgerController();
+
+            //Now start getting monthly aggregate data
+            DateTime dateTimeCursor = startDate;
+
+            while (dateTimeCursor <= endDate)
+            {
+                //Get the data
+
+                var monthValues = DataHandling.LedgerEntries.Where(e => e.Date.Month == dateTimeCursor.Month && e.Date.Year == dateTimeCursor.Year && !e.IsExpense).ToArray();
+
+                //Then group them by category and return the cat and the sum
+                var res = monthValues.GroupBy(m => m.Category).Select(cg => new { Category = DataHandling.Categories.Where(c => c.ID == cg.First().Category).Select(c => c.ID).FirstOrDefault(), Sum = cg.Sum(cg1 => cg1.Money).ToString("0.00") });
+
+                //Create a new header for each
+                rt.Headers.Add(dateTimeCursor.Year + "/" + dateTimeCursor.Month);
+
+                //Now, go through the rows, and add a new value, with the value from the res if possible, or 0 if not
+                foreach (var row in rt.Rows)
+                {
+                    var datum = res.FirstOrDefault(w => w.Category == row.CatID);
+
+                    if (datum == null)
+                    {
+                        row.Values.Add("0");
+                    }
+                    else
+                    {
+                        row.Values.Add(datum.Sum);
+                    }
+                }
+
+                dateTimeCursor = dateTimeCursor.AddMonths(1);
+            }
+
+            //Clean up, go through all cats which have no data in them, and remove them
+            for (int i = 0; i < rt.Rows.Count; i++)
+            {
+                if (!rt.Rows[i].Values.Any(v => v != "0"))
+                {
+                    //delete it
+                    rt.Rows.RemoveAt(i);
+                    i--;
+                }
+            }
+
+
+            return Ok(rt); //TODO
+        }
     }
 }
--- LazyLlamaLedger/WebApp/js/reports.js
+++ LazyLlamaLedger/WebApp/js/reports.js
@@ -35,7 +35,7 @@ function getAndCreateExpenseTable()
         headerHtml += "<td>Category</td>";
 
         for (var i = 0; i < data.Headers.length; i++) {
-            headerHtml += "<td>" + data.Headers[i] + "</td>";
+            headerHtml += "<td style='text-align:right'>" + data.Headers[i] + "</td>";
         }
 
         headerHtml += "</tr>";
@@ -50,7 +50,7 @@ function getAndCreateExpenseTable()
 
             //And the values
             for (var j = 0; j < data.Rows[i].Values.length; j++) {
-                bodyHtml += "<td>" + data.Rows[i].Values[j] + "</td>";
+                bodyHtml += "<td style='text-align:right'>" + data.Rows[i].Values[j] + "</td>";
             }
 
             bodyHtml += "</td>";
@@ -61,5 +61,43 @@ function getAndCreateExpenseTable()
         $("#tblExpenseReport thead").html(headerHtml);
         $("#tblExpenseReport tbody").html(bodyHtml);
 
+        $("#divActualReport").css("display", "block");
+
     });
+
+    $.get("http://localhost:7744/api/report/IncomeTable?StartDate=" + $("#txtDateFrom").val() + "-01" + "&EndDate=" + $("#txtDateTo").val() + "-01", function (data) {
+        var headerHtml = "<tr>";
+        //Start with the headings
+
+        headerHtml += "<td>Category</td>";
+
+        for (var i = 0; i < data.Headers.length; i++) {
+            headerHtml += "<td style='text-align:right'>" + data.Headers[i] + "</td>";
+        }
+
+        headerHtml += "</tr>";
+
+        var bodyHtml = "";
+
+        //Now do the rows
+        for (var i = 0; i < data.Rows.length; i++) {
+            bodyHtml += "<tr>";
+
+            bodyHtml += "<td style='font-weight:bold'>" + data.Rows[i].CategoryName + "</td>";
+
+            //And the values
+            for (var j = 0; j < data.Rows[i].Values.length; j++) {
+                bodyHtml += "<td style='text-align:right'>" + data.Rows[i].Values[j] + "</td>";
+            }
+
+            bodyHtml += "</td>";
+        }
+
+        //Done :) Let's create the table
+
+        $("#tblIncomeReport thead").html(headerHtml);
+        $("#tblIncomeReport tbody").html(bodyHtml);
+
+    });
+
 }
